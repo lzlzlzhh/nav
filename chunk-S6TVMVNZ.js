d88b0e15539676a82bb60761b2a1e2cf7585a653
lzlzlzhh <2847837466@qq.com>
2026-06-07 16:49:10 +0000

    Deploying to gh-pages from @ lzlzlzhh/nav@4a52f01fd2c031411dd7e6e504a27b104a8bc78a 🚀

---
+++ chunk-S6TVMVNZ.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-M7KYR3N7.js";import{ob as r,wb as e}from"./chunk-GCLNMQWI.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
